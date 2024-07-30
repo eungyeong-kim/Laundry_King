ad70396d7075caf17a069c79707a5034c78aa76e
eg_kim <kimsagwa00@gmail.com>
2024-07-30 17:53:45 +0900

    jebal

---
+++ laundryking_real/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-app-bar app dark color="blue">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>To Do List</v-toolbar-title>
+</v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ laundryking_real/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAwiETDagsxiMnT4O7IAa7hMpoj6mXjd4",
+  authDomain: "laundryking-5a8d5.firebaseapp.com",
+  projectId: "laundryking-5a8d5",
+  storageBucket: "laundryking-5a8d5.appspot.com",
+  messagingSenderId: "619955335513",
+  appId: "1:619955335513:web:0ec6d06f09212f33301c48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
