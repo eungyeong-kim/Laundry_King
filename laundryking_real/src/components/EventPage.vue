b45865986b45da25836804369fa5f62a0941e67d
유시연 <ysy6650@naver.com>
2024-08-05 18:06:03 +0900

    siyeon

--- laundryking_real/src/components/EventPage.vue
+++ laundryking_real/src/components/EventPage.vue
@@ -44,7 +44,7 @@
               <v-card
                 v-for="event in endedEvents"
                 :key="event.id"
-                :style="{ backgroundColor: event.backgroundColor, opacity: 0.2 }" 
+                :style="{ backgroundColor: event.backgroundColor, opacity: 0.5 }" 
                 class="mb-0 event-card"
                 outlined
               >
@@ -75,21 +75,23 @@ export default {
     return {
       activeTab: 0,
       ongoingEvents: [
-        { id: 1, subtitle: '품목할인', title: '대한민국 최저가<br>이불세탁 6,800원', description: '제습제도 챙겨드려요', image: 'https://via.placeholder.com/150', backgroundColor: '#DEE8F7' },
-        { id: 2, subtitle: '품목할인', title: '소중한 신발을 특별하게<br> 비브람 밑창 보강', description: '비브람 밑창 보강', image: 'https://via.placeholder.com/150', backgroundColor: '#E0F2F8' },
-        { id: 3, subtitle: '품목할인', title: '커피 한 잔 값으로<br>옷장을 넓게', description: '5,000원 할인 쿠폰이 숨어있어요', image: 'https://via.placeholder.com/150', backgroundColor: '#E0E2F8' },
-        { id: 4, subtitle: '이벤트', title: '여름 맞이 운동화 세탁<br>세탁왕으로 해결!', description: '1+1 쿠폰이 숨어있어요', image: 'https://via.placeholder.com/150', backgroundColor: '#F8ECFD' },
-        { id: 5, subtitle: '경품증정', title: '그램 단위로 구매하는<br>빈티지 스토어 ‘워셔’', description: '이젠 합리적으로 득템하세요', image: 'https://via.placeholder.com/150', backgroundColor: '#FCEAE4' },
-        { id: 6, subtitle: '경품증정', title: '세탁왕에서만 열리는<br> 리뷰 이벤트라곰!', description: '스너글로 세탁받고 선물까지!', image: 'https://via.placeholder.com/150', backgroundColor: '#E0F8EA' },
+        { id: 1, subtitle: '품목할인', title: '대한민국 최저가<br>이불세탁 6,800원', description: '제습제도 챙겨드려요', image: '/images/images/icon-bed.png', backgroundColor: '#DEE8F7' },
+        { id: 2, subtitle: '품목할인', title: '소중한 신발을 특별하게<br> 비브람 밑창 보강', description: '비브람 밑창 보강', image: '/images/images/icon-shoes.png', backgroundColor: '#E0F2F8' },
+        { id: 3, subtitle: '품목할인', title: '커피 한 잔 값으로<br>옷장을 넓게', description: '5,000원 할인 쿠폰이 숨어있어요', image: '/images/images/icon-box.png', backgroundColor: '#E0E2F8' },
+        { id: 4, subtitle: '이벤트', title: '여름 맞이 운동화 세탁<br>세탁왕으로 해결!', description: '1+1 쿠폰이 숨어있어요', image: '/images/images/icon-sneakers.png', backgroundColor: '#F8ECFD' },
+        { id: 5, subtitle: '경품증정', title: '그램 단위로 구매하는<br>빈티지 스토어 ‘워셔’', description: '이젠 합리적으로 득템하세요', image: '/images/images/icon-washer.png', backgroundColor: '#FCEAE4' },
+        { id: 6, subtitle: '경품증정', title: '세탁왕에서만 열리는<br> 리뷰 이벤트라곰!', description: '스너글로 세탁받고 선물까지!', image: '/images/images/icon-bear.png', backgroundColor: '#E0F8EA' },
       ],
       endedEvents: [
-        { id: 7, subtitle: '경품증정', title: '세탁왕으로 커피 쿠폰<br>무제한 즐기는 꿀팁', description: '오늘 커피 한 잔 어때요?', image: 'https://via.placeholder.com/150', backgroundColor: '#F7E5DE' },
-        { id: 8, subtitle: '경품증정', title: '아이폰, 에어팟 맥스<br> 당첨 주인공이 되세요!', description: '세탁왕 300만 주문 돌파 기념', image: 'https://via.placeholder.com/150', backgroundColor: '#E0F2F8' },
-        { id: 9, subtitle: '경품증정', title: '민감한 아이를 위한<br>매트리스와 소파 케어', description: '최대 100,000원 할인', image: 'https://via.placeholder.com/150', backgroundColor: '#FFC5B2' },
-        { id: 10, subtitle: '이벤트', title: '여름 맞이 운동화 세탁<br> 세탁왕으로 해결!', description: '1+1 쿠폰이 숨어있어요', image: 'https://via.placeholder.com/150', backgroundColor: '#F8ECFD' },
-        { id: 11, subtitle: '경품증정', title: '그램 단위로 구매하는<br>빈티지 스토어 ‘워셔’', description: '이젠 합리적으로 득템하세요', image: 'https://via.placeholder.com/150', backgroundColor: '#FCEAE4' },
-        { id: 12, subtitle: '경품증정', title: '세탁왕에서만 열리는<br> 리뷰 이벤트라곰!', description: '스너글로 세탁받고 선물까지!', image: 'https://via.placeholder.com/150', backgroundColor: '#E0F8EA' },
+        { id: 7, subtitle: '경품증정', title: '세탁왕으로 커피 쿠폰<br>무제한 즐기는 꿀팁', description: '오늘 커피 한 잔 어때요?', image: '/images/images/icon-coffecup.png', backgroundColor: '#F7E5DE' },
+        { id: 8, subtitle: '경품증정', title: '아이폰, 에어팟 맥스<br> 당첨 주인공이 되세요!', description: '세탁왕 300만 주문 돌파 기념', image: '/images/images/icon-iphone.png', backgroundColor: '#E0F2F8' },
+        { id: 9, subtitle: '경품증정', title: '민감한 아이를 위한<br>매트리스와 소파 케어', description: '최대 100,000원 할인', image: '/images/images/icon-sofa.png', backgroundColor: '#FFC5B2' },
+        { id: 10, subtitle: '이벤트', title: '여름 맞이 운동화 세탁<br> 세탁왕으로 해결!', description: '1+1 쿠폰이 숨어있어요', image: '/images/images/icon-sportshoes.png', backgroundColor: '#F8ECFD' },
+        { id: 11, subtitle: '경품증정', title: '그램 단위로 구매하는<br>빈티지 스토어 ‘워셔’', description: '이젠 합리적으로 득템하세요', image: '/images/images/icon-washer.png', backgroundColor: '#FCEAE4' },
+        { id: 12, subtitle: '경품증정', title: '세탁왕에서만 열리는<br> 리뷰 이벤트라곰!', description: '스너글로 세탁받고 선물까지!', image: '/images/images/icon-bear.png', backgroundColor: '#E0F8EA' },
       ]
+      
+      
     };
   },
   methods: {
@@ -98,6 +100,8 @@ export default {
     }
   }
 }
+
+
 </script>
 
 <style scoped>
@@ -126,6 +130,7 @@ export default {
   align-items: center;
   width: 100%;
   max-width: 300px;
+  height: 850px;
 }
 
 .event-card {
@@ -138,7 +143,6 @@ export default {
 }
 
 .event-image {
-  border-radius: 50px;
   width: 100%;
   object-fit: cover;
   margin-right: 30px;
