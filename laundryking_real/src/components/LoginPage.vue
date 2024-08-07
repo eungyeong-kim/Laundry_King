516920d70d2a57e08247a1eabade16c383bf655c
hyunaa-ln <hyunaaaa252@gmail.com>
2024-08-07 16:18:46 +0900

    firebase 수정

--- laundryking_real/src/components/LoginPage.vue
+++ laundryking_real/src/components/LoginPage.vue
@@ -12,7 +12,7 @@
         <v-row class="d-flex justify-center">
             <v-col cols="10">
                 <v-btn width="100%" height="49px" flat color="blue">로그인</v-btn>
-                <v-btn width="100%" height="49px" variant="outlined" color="blue" class="mt-2"><v-icon class="mr-3">mdi-google</v-icon>구글 로그인</v-btn>
+                <v-btn @click="doGoogleLogin" width="100%" height="49px" variant="outlined" color="blue" class="mt-2"><v-icon class="mr-3">mdi-google</v-icon>구글 로그인</v-btn>
                 <v-btn width="100%" height="49px" variant="text" color="blue" class="mt-2" to="/join">회원가입하기</v-btn>
             </v-col>
         </v-row>
@@ -20,12 +20,18 @@
 </template>
 
 <script setup>
+import { useStore } from 'vuex';
 import { ref } from 'vue';
 
-const rules = ref([
-    value => !!value || 'Required.',
-    value => (value && value.length >= 3) || 'Min 3 characters',
-]);
+const store = useStore();
+
+const doGoogleLogin = async () => {
+  try {
+    await store.dispatch('fnDoGoogleLogin_Popup');
+  } catch (error) {
+    console.error("로그인 중 오류 발생:", error);
+  }
+};
 </script>
 
 <style scoped>
