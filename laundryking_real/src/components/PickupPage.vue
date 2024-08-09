f8d44cf3c6aa52a699effa60e897f8ff72b41b67
hyunaa-ln <hyunaaaa252@gmail.com>
2024-08-09 18:18:53 +0900

    hyuna

--- laundryking_real/src/components/PickupPage.vue
+++ laundryking_real/src/components/PickupPage.vue
@@ -37,8 +37,14 @@
 
                 <v-row class="d-flex justify-center mt-10">
                     <v-col cols="10">
-                        <v-card variant="outlined">
-                            dd
+                        <v-card variant="outlined" color="#CCD3E8" class="d-flex align-center location-card">
+                            <img src="images/images/image3.jpg" alt="" width="100px" class="clean-img my-5 ml-5">
+                            <div class="ml-5">
+                                <h4>크린세탁 구산점</h4>
+                                <p class="mt-2"><v-icon color="#CCD3E8" class="mr-2">mdi-map-marker</v-icon>서울 은평구 연서로15길 34 1층 크린세탁</p>
+                                <p class="d-flex align-center mt-1"><v-icon color="#CCD3E8" class="mr-2">mdi-clock-outline</v-icon>9:00 ~ 20:30 <v-icon color="#CCD3E8" class="mx-2">mdi-phone</v-icon>02-338-4133</p>
+                            </div>
+                            
                         </v-card>
                     </v-col>
                 </v-row>
@@ -64,17 +70,27 @@
                 script.onload = () => {
                     const mapContainer = document.getElementById('map');
                     const mapOption = {
-                    center: new kakao.maps.LatLng(37.5665, 126.978),
-                    level: 3
+                    center: new kakao.maps.LatLng(37.611, 126.913),
+                    level: 2
                     };
                     const map = new kakao.maps.Map(mapContainer, mapOption);
                     
-                    // 마커 추가
-                    const markerPosition = new kakao.maps.LatLng(37.5665, 126.978);
-                    const marker = new kakao.maps.Marker({
-                    position: markerPosition
+                    var imageSrc = '/images/images/map-marker.png', // 마커이미지의 주소입니다    
+                        imageSize = new kakao.maps.Size(59, 59), // 마커이미지의 크기입니다
+                        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                        
+                    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                        markerPosition = new kakao.maps.LatLng(37.611, 126.913); // 마커가 표시될 위치입니다
+
+                    // 마커를 생성합니다
+                    var marker = new kakao.maps.Marker({
+                        position: markerPosition, 
+                        image: markerImage // 마커이미지 설정 
                     });
-                    marker.setMap(map);
+
+                    // 마커가 지도 위에 표시되도록 설정합니다
+                    marker.setMap(map);  
                 };
                 document.head.appendChild(script);
             }
@@ -119,5 +135,18 @@ template, div, footer{
 }
 #map {
     border-radius: 25px;
+}
+.clean-img {
+    border-radius: 10px;
+}
+
+.location-card {
+    border-radius: 15px;
+}
+.location-card h4 {
+    font-size: 18px;
+}
+.location-card p {
+    font-size: 12px;
 }
 </style>
